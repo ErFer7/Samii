28ca3c268a0e7530f4d8570d4fc48856a41a107c
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2022-08-18 01:11:42 -0300

    Testes adicionados
    Signed-off-by: Eric Fernandes Evaristo <ericfernandes7@gmail.com>

---
+++ Tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+'''
+Pacote de testes.
+'''
---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+'''
+Executa os testes.
+'''
+
+from Tests.unit_tests import MeetingTests
+
+tests = MeetingTests()
